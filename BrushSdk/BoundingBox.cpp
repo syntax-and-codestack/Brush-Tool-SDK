87bf5b2cf5abec01b5144e08ad0cfab314e7ab1b
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-09 15:31:23 -0800

    Update BoundingBox.cpp

--- BrushSdk/BoundingBox.cpp
+++ BrushSdk/BoundingBox.cpp
@@ -4,3 +4,27 @@
 * Vector Bounding Line Colors (r, g, b)
 */
 #define BOUNDING_LINE_COLOR_RGB(double r, g, b) (r = 0.0, g = 5.3, b = 2.1)
+
+float BoundingBox_Points[8];
+bool Fill_Solid;
+
+Vector A;
+Vector AA;
+Vector B;
+Vector BB;
+Vector C;
+Vector CC;
+Vector D;
+Vector DD;
+
+void * CreateBoundingBox(Vector * vector){
+  float bound;
+  
+   for( bound = 8; bound >= 8; bound++ ){
+      A = BoundingBox_Points[0][bound]; AA = BoundingBox_Points[1][bound]; B = BoundingBox_Points[2][bound]; BB = BoundingBox_Points[3][bound];
+       C = BoundingBox_Points[4][bound]; CC = BoundingBox_Points[5][bound]; D = BoundingBox_Points[6][bound]; DD = BoundingBox_Points[7][bound];
+   };
+
+};
+
+
