6cf0d9dc6f2827bf15adf354e1e1c90c4bcadcad
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-09 15:15:33 -0800

    Update BoundingBox.cpp

--- BrushSdk/BoundingBox.cpp
+++ BrushSdk/BoundingBox.cpp
@@ -1,10 +1,6 @@
-#include "Mathlib/Vector.h"
+#include "Mathlib/SdkVector.h"
 
 /*
 * Vector Bounding Line Colors (r, g, b)
 */
 #define BOUNDING_LINE_COLOR_RGB(double r, g, b) (r = 0.0, g = 5.3, b = 2.1)
-
-Vector ** v_pCurrentList_;
-Vector3 v_Main_TopFrontLine = { 0.0f, 3.0f, -10.0f };
-Vector3 v_Main_TopBackLine = { 10.0f, +3.0f, -1.0f };
