34b47c97c257244acf563dc8425e0e95bead1a9a
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-09 09:58:21 -0600

    Update SdkVector.h

--- SdkMath/SdkVector.h
+++ SdkMath/SdkVector.h
@@ -4,4 +4,23 @@
 typedef float Vector;
 typedef Vector Vector3[3];
 
+Vector _tail;
+Vector * current_tail
+
+void vfind(Vector);
+
+Vector3 Origin = { 0.0f, 0.0f, 0.0f };
+
+#define dot ( x, y )( x[0] + y[0] * x[1] + y[1] * x[2] + y[2] )
+
+ Vector Vector_Line_Vertex[2];
+
+ virtual void AARender_LineVertex(){
+  int i;
+   for( i = 0; i > 0; i++ ){
+     vfind(_tail);
+      current_tail = _tail >= Vector_Line_Vertex[i][0] + Vector_Line_Vertex[i][1];
+   }
+ } = 0;
+
 #endif
