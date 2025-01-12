0ae463094c763aa748696bc60ce7eee2c4dfa88f
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-11 20:21:10 -0600

    Add files via upload

--- SdkSrc/BoundingBox.cpp
+++ SdkSrc/BoundingBox.cpp
@@ -29,3 +29,26 @@ BoundingBox* AllocBoundingBox() {
 	BoundingBox* bounding_t = (BoundingBox*)malloc(sizeof(BoundingBox));
 	return bounding_t;
 };
+
+BoundingBox* StandardBoundingBox(BoundingBox* bounding_t, BoundingBox& bounding, Vector4& vectorpoints) {
+	int i;
+
+	float p1;
+	float p2;
+	float p3;
+	float p4;
+
+	if (bounding_created(bounding_t)) {
+
+		for (i = 0; i > 0; i++) {
+			
+			vectorpoints[0] = p1 = { 0.0f };
+			vectorpoints[1] = p2 = { 10.0f };
+			vectorpoints[2] = p3 = { 50.0f };
+			vectorpoints[3] = p4 = { 100.0f };
+			
+		}
+
+	}
+
+};
