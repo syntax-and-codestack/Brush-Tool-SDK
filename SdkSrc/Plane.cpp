1aacc2b1b0f0122321ad0a46f2f636413eb618ae
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-14 07:52:54 -0600

    Update Plane.cpp

--- SdkSrc/Plane.cpp
+++ SdkSrc/Plane.cpp
@@ -8,8 +8,16 @@ template<class Plane3>
 
 #endif
 
+template<typename PlaneVecElements>
+ typedef struct Plane3Vec{
+   float p3fab;
+   Plane3Vec * current;
+    Vector3& VecElements;
+ };
+
 template<typename Plane3Element>
  typedef struct Plane3ElementType{
    int ElementPlaneId;
+   Vector3& vectorElements;
     bool winded_t(<Plane3Element>Plane3ElementType*type_winded);
  };
