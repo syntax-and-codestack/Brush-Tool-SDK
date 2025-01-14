1be40fd0bc3e6e4900d8dd2b75e47278a7dcf3fd
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-14 09:13:51 -0600

    Create Winding.h

---
+++ SdkSrc/Winding.h
@@ -0,0 +1,13 @@
+/*Winding.h*/
+#ifndef WINDING_H
+#define WINDING_H
+
+#include "includes/SdkVector.h"
+
+template<typename DEBUG_WINDING>
+ #ifdef DEBUG_WINDING
+ #define DEBUG_WINDING
+
+ #endif
+
+#endif
