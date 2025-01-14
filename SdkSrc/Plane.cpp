043fba530afd335501a5a6a0f095eddcb17e9a3d
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-14 07:49:04 -0600

    Update Plane.cpp

--- SdkSrc/Plane.cpp
+++ SdkSrc/Plane.cpp
@@ -1,1 +1,15 @@
-#include SdkMath.h"
+#include "includes/SdkVector.h"
+
+#define PLANE_DEBUG
+#ifdef PLANE_DEBUG
+
+template<class Plane3>
+ Plane3 * pPlaneDebug;
+
+#endif
+
+template<typename Plane3Element>
+ typedef struct Plane3ElementType{
+   int ElementPlaneId;
+    bool winded_t(<Plane3Element>Plane3ElementType*type_winded);
+ };
