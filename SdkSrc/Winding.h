9e81fb716784bc251d4728a84bf42060ee0cd0f9
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-14 09:45:30 -0600

    Update Winding.h

--- SdkSrc/Winding.h
+++ SdkSrc/Winding.h
@@ -10,4 +10,19 @@ template<typename DEBUG_WINDING>
   virtual bool winding_t_debuged() = 0;
  #endif
 
+typedef class winding_t{
+ public:
+   winding_t();
+   virtual ~winding_t();
+
+   int numpoints[2];
+   Vector3 windpoints;
+
+   template<class tWindingType_t>
+    enum{ CLOCKWISE = 0, COUNTER_CLOCKWISE };
+
+using tWindingType_t = std::list<tWindingType_t, std::allocator<tWindingType_t>>;
+
+};
+
 #endif
