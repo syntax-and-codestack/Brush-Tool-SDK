c898f88d5a8a7dffc3ddbc88f7447c782865ab15
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-15 13:19:27 -0600

    Create Map.h

---
+++ SdkSrc/Map.h
@@ -0,0 +1,39 @@
+#include "Plane.h"
+#region MAP
+
+extern plane_t * active_planes;
+extern plane_t * selected_planes;
+
+class Map{
+public:
+  Map(const char * mapname, std::size_t mapsize);
+   virtual ~Map();
+
+  int MapNumberid;
+  const char * MapName;
+  bool save_map(Map * current_map);
+  bool cancel_map(Map * current_map);
+  bool load_map(Map * current_map);
+
+    virtual void constructMap(Map * map) = 0;
+
+    /*WINAPI Modules*/
+    void WINAPI ApiObservor_GetMaps(Map * map, HWND hwndDatabase);
+    void WINAPI ApiMap_GetTextures();
+    void WINAPI ApiMap_GetBrushes();
+    void WINAPI ApiMap_GetModels();
+    void WINAPI ApiMap_GetPlanes();
+    void WINAPI ApiMap_GetFaces();
+    /*?:
+      WINAPI Get Map Size
+    :?*/
+    void WINAPI * ApiMap_GetSize(){
+      std::size_t MapSize = sizeof(Map);
+    };
+
+    
+
+
+};
+
+#endregion
