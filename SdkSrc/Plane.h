3268c26cbe0657e49cd5320ec8d165662eda0db9
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-15 10:12:58 -0600

    Update Plane.h

--- SdkSrc/Plane.h
+++ SdkSrc/Plane.h
@@ -25,12 +25,35 @@ std::size_t PlaneSize = sizeof(plane_t);
 
 virtual bool resetplane(plane_t * p, winding_t * w) override = 0;
 
+enum { FREE = 0, PRIMIT = 1, WINDING = 2 }PlaneMode;
+
  plane_t * AllocPlane();
  plane_t * SplitPlane(int spilt[2], plane_t * p);
  plane_t * NewPlane();
 
+void freeplane();
+void windplane();
+
+#ifndef PLANEPRIMIT_MODE
+#define PLANEPRIMIT_MODE
+bool planeprimitmode;
+#endif
+
+bool m_bPlaneCastFree(plane_t * p, plane_t plane){
+ if(p->planeprimitmode){
+  static_cast<bool>(freeplane);
+ }
+ return p->planeprimitmode;
+};
+
 virtual void construct_plane_t(plane_t * p, winding_t * w) = 0;
 
+template<typename plane_observor_module>
+plane_observor_module plane_free(plane_t * p);
+template<typename plane_observor_module>
+plane_observor_module plane_primit(plane_t * p);
+template<typename plane_observor_module>
+plane_observor_module plane_winding(plane_t * p, winding_t * w);
 
 };
 
