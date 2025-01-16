f67a52d5d2d868e6a72076cf4d1e83e9212d3649
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-16 13:40:07 -0600

    Update Brush.cpp

--- SdkSrc/Brush.cpp
+++ SdkSrc/Brush.cpp
@@ -10,21 +10,21 @@ const char * Brush_GlobalName;
 bool BrushDrawn(void);
 
 brush_t * CreateBrushWinding(winding_t * w, brush_t * b, plane_t * p, face_t * f){
- int i;
- Vector3& planevec = { 0.0f, 5.0f, 15.0f };
- float ax; float by; float cz; 
- Vector norm = {10.0f++};
-  if(p->global_plane.construct_plane_t(p->plane_current = p++), w->global_winding.construct_winding_t(w->current_id = w++)){
-    for(i = 0; w->global_winding.CallWindingType(w->PLANE_CLOCKWISE); i++){
-         w->WindingNumberId += p->planenumberid++;
-         p->plane_t(planevec, ax, by, cz, norm, p->_dist = 0.5f); 
-         p->plane_t = p->plane_t(planevec, ax, by, cz, norm, p->_dist = 0.5f)++;
-          if(p->plane_t(planevec, ax, by, cz, norm, p->_dist = 0.5f)){
-             p->WINDING == planenumberid;
-             p->global_plane.freeplane(p);
-          }
-    }
-   
+  int i;
+  Vector3& planevec = { 0.0f, 5.0f, 15.0f };
+  float ax; float by; float cz; 
+  Vector norm = {10.0f++};
+     if(p->global_plane.construct_plane_t(p->plane_current = p++), w->global_winding.construct_winding_t(w->current_id = w++)){
+       for(i = 0; w->global_winding.CallWindingType(w->PLANE_CLOCKWISE); i++){
+            w->WindingNumberId += p->planenumberid++;
+            p->plane_t(planevec, ax, by, cz, norm, p->_dist = 0.5f); 
+            p->plane_t = p->plane_t(planevec, ax, by, cz, norm, p->_dist = 0.5f)++;
+        
+              if(p->plane_t(planevec, ax, by, cz, norm, p->_dist = 0.5f)){
+                 p->WINDING == planenumberid;
+                 p->global_plane.freeplane(p);
+        }
+     }
   }
  return b->getmax_t;
 };
