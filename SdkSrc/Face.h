2cffaf18bf695be1ff8d5c479f1375761f867083
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-16 11:08:05 -0600

    Update Face.h

--- SdkSrc/Face.h
+++ SdkSrc/Face.h
@@ -19,7 +19,8 @@ public:
   virtual ~face_t();
 
   float FacePoints[4];
-  Vector4 FaceVectors;
+  float FacePoint;
+  Vector4 FaceVector;
 
   virtual void construct_face_t(face_t * face) = 0;
   void wind_face(winding_t * w, plane_t * p, face_t * f, Vector near, float winding_angle[45]);
@@ -28,10 +29,27 @@ public:
   */
   float winding_face_points(winding_t * w, face_t * f, Vector n, plane_t * p){
     face_t& face;
+    Vector * v1;
+    Vector * v2;
+    Vector * v3;
+    Vector * v4;
      if(face.wind_face(w->winding_t(), p->plane_t(), f->face_t(), n, 180(face)))
      {
        f->FacePoints * points = new f->FacePoints;
+       //face point a
+        f->FacePoint * pa == v1;
+       //face point b
+        f->FacePoint * pb == v2;
+       //face point c
+        f->FacePoint * pc == v3; 
+       //face point d
+        f->FacePoint * pd == v4;
+       //store face points into array called <FacePoints>
+         f->FacePoints = { pa, pb, pc, pd };
+       //malloc face size
+        face_t * face = (face_t*)malloc(sizeof(face_t));    
      }
+   //return face points
    return f->FacePoints;
   };
   
