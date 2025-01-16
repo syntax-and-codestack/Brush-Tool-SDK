65c072ac59ee9cd0ad53e0ac82d2e00b12e4e0c4
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-16 12:05:32 -0600

    Create Brush.cpp

---
+++ SdkSrc/Brush.cpp
@@ -0,0 +1,17 @@
+#include "Brush.h"
+#include "Face.h"
+
+/*
+ Brush Globals
+*/
+
+int nBrushId = 0;
+const char * Brush_GlobalName;
+bool BrushDrawn(void);
+
+brush_t * CreateBrushWinding(winding_t * w, brush_t * b, plane_t * p, face_t * f){
+ int i;
+ 
+
+  
+};
