ef6f90e0e660dd8d8aaef170317eed33f09a58ce
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-17 13:04:41 -0600

    Create Plane.cpp

---
+++ SdkSrc/Plane.cpp
@@ -0,0 +1,13 @@
+/*plane.cpp*/
+#include "Plane.h"
+
+struct PlaneListNode{
+  PlaneListNode * plistnode;
+};
+
+/*
+  Create Normal Plane
+*/
+plane_t * CreateNormalPlane(plane_t * p, Vector _normal, float ax, float by, float cz, double _dist){
+
+};
