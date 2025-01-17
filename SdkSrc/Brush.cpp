20fc1b5b3909320e059171dfdf3ac152f7b78d0e
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-17 14:36:23 -0800

    Update Brush.cpp

--- SdkSrc/Brush.cpp
+++ SdkSrc/Brush.cpp
@@ -11,65 +11,61 @@ bool BrushDrawn(void);
 /*
  Brush Vars
 */
-struct BrushCVar{
-  int CBrushId;
-  char * CBrushPrimit;
-  const char * CBrushKey;
-  const char * CBrushValue;
-  typedef std::string CBrushString;
+struct BrushCVar {
+    int CBrushId;
+    const char* CBrushPrimit;
+    const char* CBrushKey;
+    const char* CBrushValue;
+    typedef std::string CBrushString;
 };
 
 /*
  Brush List Node
 */
-struct BListNode{
-  BListNode * brush_list_node;
+struct BListNode {
+    BListNode* brush_list_node;
 };
 
 /*
  Create Brush Points
 */
-brush_t * BrushPoints(brush_t * b){
- static char cBuff[1024];
-  if(b->b_pBrushPrimitMode){
-     b->BrushPoints = 8;
-      printf("%f", b->BrushPoints);
-      b->global_brush.FreeBrush(b);
-  }
- return b->current_brush(b);
+brush_t* BrushPoints(brush_t* b) {
+    static char cBuff[1024];
+    if (b->b_pBrushPrimitMode) {
+        b->BrushPoints = 8;
+        printf("%f", b->BrushPoints);
+        b->global_brush.FreeBrush(b);
+    }
+    return b->current_brush(b);
 };
 
 /*
  Brush Sides Label
 */
-brush_t * BrushSides_Label(brush_t * b, BrushCVar * brush_c){
- float brush_sides[6];
-  brush_c->CBrushPrimit = 'c';
- 
- return b->current_brush(b);
+brush_t* BrushSides_Label(brush_t* b, BrushCVar* brush_c) {
+    float brush_sides[6];
+    brush_c->CBrushPrimit = "c";
+
+    return b->current_brush(b);
 };
 
 /*
  Brush Top Face
 */
-brush_t * BrushTop_Face(brush_t * b, face_t * f, winding_t * w, plane_t * p){
- plane_t * top_plane = p;
- winding_t * plane_winding = w;
+brush_t* BrushTop_Face(brush_t* b, face_t* f, winding_t* w, plane_t* p) {
+    plane_t* top_plane = p;
+    winding_t* plane_winding = w;
 
-  int i;
+    int i;
 
-   for(i = 0; i >= 0; i++){
-     p->plane_current = top_plane;
-       top_plane->NewPlane(top_plane);
-       top_plane->planevectors = { 0.0f, 5.0f, 15.0f };     
-   };
+    for (i = 0; i >= 0; i++) {
+        p->plane_current = top_plane;
+            top_plane->NewPlane();
+    }
 
- top_plane->global_plane.freeplane(top_plane);
- 
- return b->brush_plane(b, plane_winding, top_plane);
+    top_plane->global_plane.freeplane(top_plane);
+
+    return b->brush_plane(b, plane_winding, top_plane);
 };
-
-
-
 
 
