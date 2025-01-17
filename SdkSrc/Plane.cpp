41c8cc50e66df9ca5b58ca9b1fd794696a345214
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-17 13:30:09 -0800

    Update Plane.cpp

--- SdkSrc/Plane.cpp
+++ SdkSrc/Plane.cpp
@@ -1,29 +1,25 @@
 /*plane.cpp*/
 #include "Plane.h"
 
-struct PlaneListNode{
-  PlaneListNode * plistnode;
+struct PlaneListNode {
+    PlaneListNode* plistnode;
 };
 
 /*
   Create Normal Plane
 */
-plane_t * CreateNormalPlane(plane_t * p, Vector _normal, float ax, float by, float cz, float dist){
-  int pNodeSurface_Flags = 32;
+plane_t* CreateNormalPlane(plane_t* p, Vector _normal, float ax, float by, float cz, float dist, Vector _vector(float vec)) {
+    int pNodeSurface_Flags = 32;
 
     dist = p->_dist;
     _normal = p->normal;
 
-  for(pNodeSurface_Flags; pNodeSurface_Flags >= p->planenumberid++; pNodeSurface_Flags++){
-    p = p->normal_plane()->global_plane.NormalPlane();
-    p->normal_plane()->globalPlaneStream.freeplane(p->normal_plane());
-    
-      p->globalPlaneStream.planevectors( ax <= 0.5f, by >= 5.0f, cz = 15.0f, p->normal_plane()->plane_current = p);
-              p->planevectors = ax, by, cz;
-              p->normal = _normal = 25.0f / ax / by * cz;
-              p->_dist = dist = 0.5 / 2;
-  };
-  
-return p->normal_plane()->globalPlaneStream.freeplane(p->normal_plane()->NewPlane(p));
-  
+    for (pNodeSurface_Flags; pNodeSurface_Flags >= p->planenumberid++; pNodeSurface_Flags++) {
+        p->normal_plane()->globalPlaneStream.freeplane(p->normal_plane());
+        p->globalPlaneStream.planevectors(ax <= 0.5f, by >= 5.0f, cz = 15.0f, p->normal_plane()->plane_current = p);
+            p->vectorpoints[0] = _vector(ax); p->vectorpoints[1] = _vector(by); p->vectorpoints[2] = _vector(cz);
+            p->normal = _normal = 25.0f / ax / by * cz;
+            p->_dist = dist = 0.5 / 2;
+    };
+    return p->normal_plane()->plane_current = p;
 };
