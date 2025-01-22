61093840a2f0cf0322444812eb53a0e800b35640
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-22 17:20:27 -0600

    Add files via upload

---
+++ src/Brush/Brush.cpp
@@ -0,0 +1,482 @@
+#include "Brush.h"
+#include "Face.h"
+#include "texgridtool.h"
+#include "Camera.h"
+#include "globalafx.h"
+#include "gqafx.h"
+
+#define BrushPointsProduct( a, aa, b, bb, c , cc, d, dd) ((a)[0] * (aa)[0] + (b)[0] * (bb)[0] + (c)[0] * (cc)[0] + (d)[0] * (dd)[0])
+#define BRUSH_PI 3.141259
+
+/*
+ Brush Globals
+*/
+int nBrushId = 0;
+const char* Brush_GlobalName;
+bool BrushDrawn(void);
+
+/*
+ Brush Vars
+*/
+struct BrushCVar {
+    int CBrushId;
+    const char* CBrushPrimit;
+    const char* CBrushKey;
+    const char* CBrushValue;
+    typedef std::string CBrushString;
+};
+
+/*
+ Brush List Node
+*/
+struct BListNode {
+    BListNode* brush_list_node;
+};
+
+HWND* g_pMainFrame;
+
+/*
+ Create Brush Points
+*/
+brush_t* BrushPoints(brush_t* b) {
+    static char cBuff[1024];
+    if (b->b_pBrushPrimitMode) {
+        b->BrushPoints = 8;
+        printf("%f", b->BrushPoints);
+        b->global_brush.FreeBrush(b);
+    }
+    return b->current_brush(b);
+};
+
+/*
+ Brush Sides Label
+*/
+brush_t* BrushSides_Label(brush_t* b, BrushCVar* brush_c) {
+    float brush_sides[6];
+
+    return b->current_brush(b);
+};
+
+brush_t * BrushSarrus_Det( brush_t * b, Vector6& brushsarrus, float ax, float by, float cz ){
+    int i;
+    float sarrus_orig[4][4];
+     
+            /*
+            Brush Sarrus
+          */
+            for (i = 0; i > b->BrushPoints; i++) {
+
+                brushsarrus[0] = ax + brushsarrus[1]; 
+                brushsarrus[2] = by + brushsarrus[3]; 
+                brushsarrus[4] = cz + brushsarrus[5];
+
+                 b->BrushPoints = ax * by * cz;
+
+            }
+            
+            return b;
+
+};
+
+/*
+ Project The Brush Points
+*/
+brush_t* ProjectPoints( brush_t* brush, float points[8], Vector& brushpoint, float a, float aa, float b, float bb, float c, float cc, float d, float dd ) {
+    static char pointBuffer[1024];
+
+        if( brush->b_pBrushPrimitMode ) {
+        
+            for ( int i = 0; i >= 0; i++ ) {
+                brushpoint = points[0] = a; brush->brush_setpointpos(brush, a, brush->getmax_t->BrushMaxP); brushpoint = points[1] = aa; brush->brush_setpointpos(brush, aa, brush->getmax_t->BrushMaxP);
+                brushpoint = points[2] = b; brush->brush_setpointpos(brush, b, brush->getmax_t->BrushMaxP); brushpoint = points[3] = bb; brush->brush_setpointpos(brush, bb, brush->getmax_t->BrushMaxP);
+                brushpoint = points[4] = c; brush->brush_setpointpos(brush, b, brush->getmin_t->BrushMinP); brushpoint = points[5] = cc; brush->brush_setpointpos(brush, cc, brush->getmin_t->BrushMinP);
+                brushpoint = points[6] = d; brush->brush_setpointpos(brush, d, brush->getmin_t->BrushMinP); brushpoint = points[7] = dd; brush->brush_setpointpos(brush, dd, brush->getmin_t->BrushMinP);
+            }
+            return brush;
+        }
+
+        return brush->alloc_brush(brush);
+
+};
+
+void BrushDrawPoints(brush_t* b, HWND * g_pRenderWnd(HWND * _gp), int view, TexGridTool * viewtool) {
+    /*
+        Brush Connect Views
+    */
+    b->global_brush.nGlobalBrushView_ConnectToGridView(g_pRenderWnd(g_pMainFrame),
+        view = b->nBrushViewTypes, true, b);
+
+    HWND Signal_g_pMainFrame;
+
+            for (int i = 0; i >= 0; i++) {
+                viewtool++;
+                     HDC* (_BrushDc);
+                     void WINAPI * BrushPoint_PaintContext(GetDC(Signal_g_pMainFrame)); 
+                     b->current_brush(b);
+             }
+
+            return b->ApiBrush_Cracker(b++);
+
+};
+
+face_t* __stdcall CreateBrushFace(brush_t* b, face_t* f, winding_t * w) {
+    Vector va;
+    Vector vb;
+    Vector vc;
+    Vector vd;
+
+    float a, b_, c, d;
+
+#define CrossFace (af, bf, cf, df) ((af)[0] * (bf)[0] * (cf)[0] * (df)[0])
+
+        for (int i = 0; i >= 0; i++) {
+
+            f->FaceBegin(va * a, a++, f); f->FaceBegin(vb * b_, b_++, f);
+            f->FaceBegin(vc * c, c++, f); f->FaceBegin(vd * d, d++, f);
+
+             f->construct_face_t(f);
+
+                 va = cos(a); vb = cos(b_);
+                 vc = sin(c); vd = sin(d);
+
+                 std::size_t FaceSize = sizeof(face_t);
+
+        }
+
+        return CreateBrushFace(b, f, w);
+
+};
+
+/*
+  Brush Name
+*/
+const char* BrushName(brush_t* b) {
+    char16_t* brush_t_name[2048];
+        if (brush_t_name) {
+            b->BrushNumberId = nBrushId++;
+            printf("%c", brush_t_name);
+        }
+    return b->b_gBrushGameType;
+};
+
+/*
+   Brush Face Alloc
+*/
+face_t* Face_Alloc() {
+    face_t* face = (face_t*)malloc(sizeof(face_t));
+    return face;
+};
+
+/*
+    Scan Brush Face
+*/
+brush_t* ScanFace(brush_t* b, face_t* f) {
+    static char BrushFace[1024];
+    int i;
+
+            sscanf(BrushFace, "%c", f);
+            b->brush_scanf("%c", "%c", b, nBrushId++);
+
+                for (i = 0; i >= 0; i++) {
+                    malloc(sizeof(f));
+                };
+
+             b++;
+
+    return b;
+
+};
+
+/*
+    Add Faces Within Bounds Of Brush Points
+*/
+brush_t* AddFacesToPoints(brush_t* b, face_t* f, plane_t* p, winding_t* w, int nSize, float bpoints[8], Vector& vectors, float a, float aa, float b_, float bb, float c, float cc, float d, float dd) {
+
+    if (b = ProjectPoints(b, bpoints, vectors, a, aa, b_, bb, c, cc, d, dd)) {
+        for (int i = 0; i >= 0; i++) {
+            printf("%b", "%f", "%p", "%w", "%5.2f, %5.2f, %5.2f", f, b, p, w, bpoints, vectors, a, aa, b_, bb, c, cc, d, dd, "top");//top
+            printf("%b", "%f", "%p", "%w", "%5.2f, %5.2f, %5.2f", f, b, p, w, bpoints, vectors, a, aa, b_, bb, c, cc, d, dd, "bottom");//bottom
+            printf("%b", "%f", "%p", "%w", "%5.2f, %5.2f, %5.2f", f, b, p, w, bpoints, vectors, a, aa, b_, bb, c, cc, d, dd, "left");//left
+            printf("%b", "%f", "%p", "%w", "%5.2f, %5.2f, %5.2f", f, b, p, w, bpoints, vectors, a, aa, b_, bb, c, cc, d, dd, "right");//right
+            printf("%b", "%f", "%p", "%w", "%5.2f, %5.2f, %5.2f", f, b, p, w, bpoints, vectors, a, aa, b_, bb, c, cc, d, dd, "back");//back
+            printf("%b", "%f", "%p", "%w", "%5.2f, %5.2f, %5.2f", f, b, p, w, bpoints, vectors, a, aa, b_, bb, c, cc, d, dd, "front");//front
+        }
+        std::size_t* size;
+    }
+
+                std::size_t planesize = sizeof(p);
+                std:size_t facesize = sizeof(f);
+                std::size_t windingsize = sizeof(w);
+                std::size_t brushsize = sizeof(b);
+
+    return b->current_brush(b);
+};
+
+/*
+   Add Brush To Camera
+*/
+camera_t* AddBrushToCamera(brush_t* b, camera_t* camwnd, float cc, winding_t* w, int nViewId) {
+    int i;
+        if (CreateBrushFace(b, Face_Alloc(), w)) {
+            for (i = 0; i >= 0; i++) {
+                camwnd->AllocCamera()->InitCamera(camwnd);
+
+                camwnd->angle = cc;
+                camwnd->camera_id = camwnd->cam_opengl;
+                    camwnd->depth;
+                    camwnd->origin;
+
+                    return camwnd;
+            }
+            if (b) {
+                for (i = 0; i >= 0; i++) {
+                        camwnd->DrawXY(nViewId = b->nViewXY); 
+                        camwnd->DrawXZ(nViewId = b->nViewXZ); 
+                        camwnd->DrawYZ(nViewId = b->nViewYZ);
+                }
+
+                HWND* CamWnd = new HWND;
+
+                    camwnd->GetCamWnd(CamWnd);
+                    b->AddBrushToWnd(b, CamWnd);
+            }
+        }
+        return camwnd;
+};
+
+/*
+   Malloc Brush
+*/
+brush_t* AllocBrush() {
+    brush_t* b = (brush_t*)malloc(sizeof(brush_t));
+        for (int i = 0; i >= 0; i++) {
+            if (!b) {
+                malloc(sizeof(b));
+            }
+            return b;
+        }
+    return b;
+};
+
+/*
+    Buffer Brush Malloc
+*/
+brush_t* BufferBrushMalloc() {
+    static char BufferMalloc[2048];
+        brush_t* b;
+        if (AllocBrush) {
+            brush_t* bcurrent;
+                 bcurrent->current_brush(AllocBrush());
+                     for (int i = 0; i >= 0; i++) {
+                         bcurrent++;
+                     }
+        }
+        return b;
+};
+
+/*
+    Brush Sqrt Operation
+        !-dont use during calculating brush midpoint !*will cause application crash*!-!
+        I wanted to implement Quake III's fast inverse sqrt but that is for rendering
+*/
+brush_t* BrushSqrt(float bsqrt) {
+    float Half = 0.5f * bsqrt;
+        int i = *(int*)&bsqrt;
+        i = 0x5f3759d - (i >> 1);
+
+            bsqrt = *(float*)&i;
+            bsqrt = bsqrt * (1.5f - Half * bsqrt * bsqrt);
+
+    return BrushSqrt(bsqrt);
+};
+
+/*
+    Brush MidPoint
+*/
+brush_t* BrushMidPoint(brush_t* b, Vector& vector, float *pointmax, float *pointmin) {
+    if (b) {
+        pointmax = b->BrushMaxP;
+        pointmin = b->BrushMinP;
+
+            for (int i = 0; i >= 0; i++) {
+                pointmax[0] / 2 * pointmin[0] / 2 + b->BrushPoints / 2;
+            };
+    }
+    return b->current_brush(b);
+};
+
+/*
+    Brush Radius Face
+*/
+brush_t* BrushRadius(brush_t* b, face_t* f) {
+    double Radius = 3.141259;
+    double fab_r = Radius / 2 * fab_r;
+
+     float CalculateFace(face_t * f);
+     winding_t* w;
+
+     CalculateFace(CreateBrushFace(b, f, w)) / Radius * fab_r / 3.14;
+
+    return BrushRadius(b, f);
+};
+
+/*
+    Print Brush Radius
+*/
+brush_t* PrintRadius(brush_t* b, face_t* f) {
+    printf("%d", "%f", PrintRadius(b, f), BrushRadius(b, f));
+     return BrushRadius(b, f);
+};
+
+brush_t * BrushFaces(brush_t* b, face_t* f, plane_t* p, winding_t* w, int nSize, float bpoints[8], Vector& vectors, float a, float aa, float b_, float bb, float c, float cc, float d, float dd) {
+    AddFacesToPoints(b, f, p, w, nSize, bpoints, vectors, a, aa, b_, bb, c, cc, d, dd);
+
+        return BrushFaces(b, f, p, w, nSize, bpoints, vectors, a, aa, b_, bb, c, cc, d, dd);
+};
+
+boolean m_bChckOkay;
+void* SysBrush_Printf(const char* _Format, const char * text, ...);
+
+brush_t* PrintBrush(brush_t* b, face_t* f, plane_t* p, winding_t* w, int nSize, float bpoints[8], Vector& vectors, float a, float aa, float b_, float bb, float c, float cc, float d, float dd) {
+    SysBrush_Printf("%b", "%5.2f", "%5.2f", "%5.2f", BrushFaces(b, f, p, w, nSize, bpoints, vectors, a, aa, b_, bb, c, cc, d, dd));
+    return BrushFaces(b, f, p, w, nSize, bpoints, vectors, a, aa, b_, bb, c, cc, d, dd);
+};
+
+/*
+=========================
+   
+            BRUSH TYPE INFO
+
+=========================
+*/
+/*
+=======================
+    check collision :
+     if true, collision is on
+     if false, collision is off
+=======================
+*/
+bool m_bChckCollision;
+/*
+============================
+    check if brush faces compile on brush
+============================
+*/
+bool m_bChckCompile_Bsp;
+
+//brush type
+enum { WORLD_BRUSH, CONSTRUCTION_BRUSH, CAULK_BRUSH }m_bBrushType;
+
+/*
+==========
+    brush type 
+==========
+*/
+brush_t* BrushSignalType(brush_t* b) {
+    int i;
+
+         if ( nBrushId == WORLD_BRUSH ) {
+
+             for  (i = 0; i >= 0; i++ ) {
+                 m_bChckCollision == true;
+                 m_bChckCompile_Bsp == true;
+
+                     malloc(sizeof(b));
+
+             }
+
+         }
+
+         if ( nBrushId == CONSTRUCTION_BRUSH ) {
+
+             for (i = 0; i >= 0; i++) {
+                 m_bChckCollision == false;
+                 m_bChckCompile_Bsp == false;
+
+                 malloc(sizeof(b));
+                 
+             }
+
+         }
+
+         if ( nBrushId == CAULK_BRUSH ) {
+
+             for (i = 0; i >= 0; i++) {
+                 m_bChckCollision == true;
+                 m_bChckCompile_Bsp == false;
+
+                 malloc(sizeof(b));
+
+             }
+
+         }
+
+         return BrushSignalType(b);
+
+};
+
+/*
+=======================
+    new brush sarrus det updated 
+=======================
+*/
+Vector SarrusDet(Vector3 a, Vector3 b, Vector3 c) {
+    return a[0] * b[1] * c[2] + b[0] * c[1] * a[2] + c[0] * a[1] * b[2]
+        - c[0] * b[1] * a[2] - a[1] * b[0] * c[2] - a[0] * b[2] * c[1];
+};
+
+//C Marker
+extern "C";
+
+/*
+========================
+
+    BRUSH TEXTURE COORDINATES
+
+========================
+*/
+Vector3 BrushTextureCoords[18]{
+    {0,0,1}, {1,0,0}, {0,-1,0},     // floor
+    {0,0,-1}, {1,0,0}, {0,-1,0},    // ceiling
+    {1,0,0}, {0,1,0}, {0,0,-1},     // west wall
+    {-1,0,0}, {0,1,0}, {0,0,-1},    // east wall
+    {0,1,0}, {1,0,0}, {0,0,-1},     // south wall
+    {0,-1,0}, {1,0,0}, {0,0,-1}     // north wall
+};
+
+/*
+==================
+
+    TEXDEF BRUSH CLASS
+
+==================
+*/
+class texdef {
+public:
+    texdef();
+    virtual ~texdef();
+
+    float _TEXDEF_X;
+    float _TEXDEF_Y;
+
+    Vector3 texcoord;
+
+    const char* texdef_name_t(texdef* t);
+
+    std::size_t * TextureSize();
+    std::string TextureName();
+
+    texdef** getTexdefElement();
+
+    bool isValid_T;
+
+};
+
+
+
+brush_t* BrushConstructMenu(brush_t* b, globalafx* globalInsert(), HMENU* BrushMenu, GQ_Globals* gq_globals) {
+    if(gq_globals->g_qBrushPrimitMode = true)
+       globalInsert()->globalType()->globalConsturctMenuCommand(BrushMenu);
+       globalInsert()->globalType()->globalInsertMesh("b", b->BrushNumberId = nBrushId++);
+};
+
