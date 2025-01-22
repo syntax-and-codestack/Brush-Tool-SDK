0a6fc83ba0c04b3dfd6a353e522198b0d45ad33d
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-22 17:11:01 -0600

    Add files via upload

--- win32/winapi.h
+++ win32/winapi.h
@@ -11,7 +11,7 @@
    BOOLEAN,
    FLOAT
 
-  &&other common win32 resources  
+  &&other common win32 resources
 */
 
 #include <Windows.h>
@@ -21,27 +21,22 @@
 typedef HWND WinWidget;
 typedef HINSTANCE WinInstance;
 typedef MSG WinMsg;
-typedef WINAPI CallApi;
 typedef HICON WinIcon;
 typedef HMENU WinMenu;
-typedef HTOOLBAR WinToolbar;
 
-WinWidget * globalWidget();
-void * Window32(WinWidget * _widget);
+WinWidget* globalWidget();
+void* Window32(WinWidget* _widget);
 
-WinInstance * globalInstance();
-void * Instance32(WinInstance * _instance);
+WinInstance* globalInstance();
+void* Instance32(WinInstance* _instance);
 
-WinMsg * globalMsg();
-void * Msg32(WinMsg * _msg);
+WinMsg* globalMsg();
+void* Msg32(WinMsg* _msg);
 
-WinIcon * globalIconPaint();
-void * IconPaint(WinIcon * _icon);
+WinIcon* globalIconPaint();
+void* IconPaint(WinIcon* _icon);
 
-WinMenu * globalMenu();
-void * Menu(WinMenu * _menu);
-
-WinToolbar * globalConstructToolbar();
-void * Toolbar(WinToolbar * _toolbar);
+WinMenu* globalMenu();
+void* Menu(WinMenu* _menu);
 
 #endif
