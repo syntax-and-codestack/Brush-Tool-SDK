1fb70e29e9dc7b615b288eae580d5b6b260375ed
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-24 14:05:37 -0800

    Update BrushModule.cpp

--- src/Brush/BrushModule.cpp
+++ src/Brush/BrushModule.cpp
@@ -3,31 +3,31 @@
 std::FILE* BrushModule_File;
 
 const void Module_BrushFile(brush_t * b) {
-	b++;
-	 BrushModule_File->_Placeholder = b;
-	 return Module_BrushFile(b);
+   b++;
+     BrushModule_File->_Placeholder = b;
+    return Module_BrushFile(b);
 }const;
 
 void Brush_ModuleSignal(brush_t* b) {
 
-		if (b->b_pBrushPrimitMode) {
+	if (b->b_pBrushPrimitMode) {
 
-			auto& CreateBrushModule(b);
+		auto& CreateBrushModule(b);
 
-			b->BrushMaxP++;
-			b->BrushMinP++;
-		}
+		b->BrushMaxP++;
+		b->BrushMinP++;
+	}
 
-		if (b) {
-			b++;
-			free(b);
-		}
+	if (b) {
+		b++;
+		free(b);
+	}
 
-			int i;
+		int i;
 
-		for (i = 0; i >= 0; i++) {
-			auto& CreateBrushModule();
-		}
+	for (i = 0; i >= 0; i++) {
+		auto& CreateBrushModule();
+	}
 
 	return b->FreeBrush(b);
 
