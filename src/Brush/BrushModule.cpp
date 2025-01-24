8f94451a1f6f05b5092a743459934e96a7747f1f
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-24 15:58:29 -0600

    Add files via upload

---
+++ src/Brush/BrushModule.cpp
@@ -0,0 +1,27 @@
+#include "Brush.h"
+
+void Brush_ModuleSignal(brush_t* b) {
+
+		if (b->b_pBrushPrimitMode) {
+
+			auto& CreateBrushModule(b);
+
+				b->BrushMaxP++;
+				b->BrushMinP++;
+		}
+
+		if (b) {
+			 b++;
+			free(b);
+		}
+
+		int i;
+
+			for (i = 0; i >= 0; i++) {
+				auto& CreateBrushModule();
+			}
+
+		return b->FreeBrush(b);
+
+
+}
