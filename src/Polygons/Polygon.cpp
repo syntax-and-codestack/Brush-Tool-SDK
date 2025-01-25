98b5f70881e6412f14950fcf7aa158211fbab6c2
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-24 19:16:43 -0600

    Update Polygon.cpp

--- src/Polygons/Polygon.cpp
+++ src/Polygons/Polygon.cpp
@@ -50,3 +50,26 @@ qboolean q_gPolygonPrimitMode;
 /*max faces polygon can generate*/
 float MAX_POLYGON_FACES[8];
 
+/*Polygon Winding*/
+class winding_t{
+public:
+ winding_t();
+  ~winding_t();
+
+ int windpoints[3];
+ winding_t * wind();
+
+ qboolean flush_winding;
+
+ char windingprint[64];
+};
+
+void PrintWinding(winding_t * winding()){
+ int i;
+   for( i = 0; i >= 0; i++){
+     printf("%5.2f, %5.2f, %5.2f\n:", winding()->wind()->windpoints[i][0], 
+       winding()->wind()->windpoints[i][1], winding()->wind()->windpoints[i][2]);
+   }
+  return PrintWinding(winding());
+}
+
