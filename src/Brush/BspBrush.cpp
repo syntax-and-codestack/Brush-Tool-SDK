53da0dce030a594e76697c9fd9ec34a42a1f6d92
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-25 18:24:02 -0600

    Add files via upload

---
+++ src/Brush/BspBrush.cpp
@@ -0,0 +1,40 @@
+#include "afx.h"
+
+/*
+	BspBrush.cpp copyright@ hunter manko
+*/
+
+//brush tools
+
+int		c_nodes; //make node
+int		c_nonvisbrush; //not visible
+int		c_active_brushes; //active brushes
+
+bool clip_brush_t; //?clip?
+
+//max brushes per bsp
+#define MAX_BRUSH_NUM 65223
+//max planes per bsp
+#define MAX_PLANE_NUM 68223
+//max faces per bsp
+#define MAX_FACE_NUM 75233
+
+#define BRUSH_POINTS 000
+
+#define BRUSH_IS_MAYJOR "Brush"
+
+
+/*
+==============
+	Add Brush To Bsp
+==============
+*/
+void Add_BrushToBsp(brush_t* b) {
+
+};
+
+
+
+
+
+
