3c2ed312e9aade8eb7670a464fe726678a10fc9a
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-27 11:27:41 -0600

    Update BrushPrimit.cpp

---
+++ source/EditorCommon/BrushPrimit.cpp
@@ -0,0 +1,11 @@
+//BrushPrimit.cpp copyright@ hunter manko
+
+#include "afx.h"
+#include <limits.h>
+
+void Construct_BrushCuboid(brush_t * brush, float Bounds[8], float MinBounds[4], float MaxBounds[4]){
+ int i;
+  for( i = 0; brush; brush++){
+    Bounds[i][0] = 
+  }
+};
