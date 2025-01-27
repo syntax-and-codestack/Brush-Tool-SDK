0684b8ebe57ffc37b843b3cbc1b9d441280639e1
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-27 07:43:29 -0600

    Update Axis.h

--- source/EditorCommon/Axis.h
+++ source/EditorCommon/Axis.h
@@ -1,1 +1,34 @@
+/*
+ Axis.h copyright@ hunter manko
+*/
 #pragma once
+#ifndef AXIS_H
+#define AXIS_H
+
+//Axis Defines
+AXIS_X 000
+AXIS_Y 111
+AXIS_Z 015
+
+/*
+
+  y
+  |
+  |
+  |
+  |
+  |
+  |
+  |
+  |
+  |_______________________ x
+  /
+ /
+/
+z
+
+
+
+*/
+
+#endif
