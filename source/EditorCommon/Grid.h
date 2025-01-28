f5883ade0762783adab203072000176bc635cde6
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-28 09:58:21 -0600

    Update Grid.h

--- source/EditorCommon/Grid.h
+++ source/EditorCommon/Grid.h
@@ -1,1 +1,35 @@
 #pragma once
+#ifndef GRID_H
+#define GRID_H
+
+//Grid.h copyright@ hunter manko
+
+template<typename GridPanel>class CGrid{
+public:
+ CGrid();
+ ~CGrid();
+
+ int x;
+ int y;
+
+ #define PAINT_PANEL_GRID 101
+
+ enum{
+  MFC = 0,
+  WIN32
+ }GridPanelWidgetOwner;
+
+virtual void Paint() = 0;
+
+};
+
+class Grid{
+public:
+ Grid();
+  ~Grid();
+
+ std::size_t g_nGridSize = sizeof(Grid);
+
+};
+
+#endif
