974699e69649ea9fa9be1d4b7bb2f16741727feb
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-29 10:07:10 -0600

    Create cgl.h

---
+++ source/EditorCommon/cgl.h
@@ -0,0 +1,16 @@
+//cgl.h copyright@ hunter manko
+#pragma once
+#ifndef CGL_H
+#define CGL_H
+
+typedef float VColorId[4];
+#define CGL_PEN_RED
+#define CGL_PEN_WHITE
+#define CGL_PEN_GREEN
+VColorId * vecc_id;
+HGLRC CGL_GL_CONTEXT;
+HGLRC BRUSH_HIGHLIGHT;
+HGRLC ENTITY_HIGHLIGHT;
+HGRLC MODEL_HIGHLIGHT;
+
+#endif
