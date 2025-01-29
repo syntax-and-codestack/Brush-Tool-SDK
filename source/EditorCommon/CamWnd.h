797b748344fb4d7fabda60d61534b13913c3ce6c
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-29 07:41:37 -0600

    Update CamWnd.h

--- source/EditorCommon/CamWnd.h
+++ source/EditorCommon/CamWnd.h
@@ -20,20 +20,6 @@ public:
 	HDC g_pCamDC = GetDC(g_pCamWnd);
 	HGLRC g_pCamWndGlContext = wglCreateContext(g_pCamDC);
 
-	typedef PIXELFORMATDESCRIPTOR PFN_CAMERAPIXELTABLE;
-
-	void CreateCameraWndPixelTable(CamWnd* Cam) {
-		PFN_CAMERAPIXELTABLE CamWndPixels{
-			CamWndPixels.nSize = CameraObservor()->m_bCamWndId = sizeof(Cam),
-			CamWndPixels.bReserved = m_bCamWndId--,
-			CamWndPixels.cAlphaBits = 16,
-				CamWndPixels.cColorBits = 32,
-				CamWndPixels.cDepthBits = 64,
-				CamWndPixels.cStencilBits = 16 * 32,
-				CamWndPixels.iPixelType = 6
-		};
-	};
-
 	std::size_t CamWndSize = sizeof(CamWnd);
 
 	virtual void ReleaseCamWnd() = 0;
