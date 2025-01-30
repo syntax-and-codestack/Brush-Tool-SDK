3bd14faf8cefe657b591f507073bd7c7d9fa4443
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-30 17:57:59 -0600

    Create PolarBrush.cpp

---
+++ source/EditorCommon/PolarBrush.cpp
@@ -0,0 +1,37 @@
+/*Polar Brush Includes*/
+#include "afx.h
+#include "Stdafx.h"
+
+#define BRUSH_POLAR_VELOCITY (dr, dt, rr, r, 00) ((dr)[0] / (dt)[0] = (rr)[0] + ((r)[0](00)[0])
+
+#define BRUSH_POLAR_NORTH (unsigned double N) ((N)[0] = 90.0)
+#define BRUSH_POLAR_EAST (unsigned double E) ((E)[0] = 0.0)
+#define BRUSH_POLAR_SOUTH (unsigned double S ((S)[0] = 270.0)
+#define BRUSH_POLAR_WEST (unsigned double W) ((W)[0] = 180.0)
+
+#define POLAR_RADIUS (double r)
+
+/*
+
+               Y
+               |
+      p.       |
+        \      |
+         \     |
+          \ r  | 0
+           \---|\
+            \  | \
+<-------------------------------->X
+               |
+               |
+               |
+               |
+               |
+               |
+               |
+
+
+
+
+
+*/
