e077c1f81573f7e0ada0ac5ee0bbb94d0941ad9c
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-29 20:14:10 -0600

    Add files via upload

--- includes/Stdafx.h
+++ includes/Stdafx.h
@@ -5,22 +5,19 @@
 
 //typedefs
 typedef bool qboolean;
-typedef malloc qmalloc;
+void* _cdecl qmalloc(std::size_t _Size);
 
-void qsysprintf(const char * text, ...);//work as printf function
-void qsprintf(char * buffer, const char * format, int * id);//work as a sprintf function
+void qsysprintf(const char* text, ...);//work as printf function
+void qsprintf(char* buffer, const char* format, int* id);//work as a sprintf function
 
 //globals
-class _q_eglobals{
+class _q_eglobals {
 public:
- qboolean qselect;
- qboolean qdraw_t;
- qboolean qbrushselected;
+	qboolean qselect;
+	qboolean qdraw_t;
+	qboolean qbrushselected;
 
-void qsysprintf(const char * text, ...);//work as printf function
-void qsprintf(char * buffer, const char * format, int * id);//work as a sprintf function
-
-qboolean qbrushprimit_mode;
+	qboolean qbrushprimit_mode;
 };
 
 #endif
