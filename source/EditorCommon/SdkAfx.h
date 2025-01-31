362d1ca5f29080c44be32a6b462c51a26c2fb04e
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-31 13:14:16 -0600

    Update SdkAfx.h2

--- source/EditorCommon/SdkAfx.h
+++ source/EditorCommon/SdkAfx.h
@@ -16,7 +16,7 @@ public:
 int m_bMouseClickId;
 
 virtual void AppRegister_MouseRightClickCommand() = 0;
-virtual void AppRegister_MouseLeftClickCommand() = 0;
+virtual void AppRegister_MouseLeftClickCommand() = 0
 virtual void AppRegister_MouseMiddleClickCommand() = 0;
 virtual void AppRegister_KeyboardKeyClickCommand(const char * Key) = 0;
 virtual void AppRegister_CursorCommand(HCURSOR cursor) = 0;
@@ -24,10 +24,13 @@ virtual void AppRegister_WindowCommand(HWND hwnd) = 0;
 virtual void AppRegister_ButtonCommand(HWND button) = 0;
 virtual void AppRegister_WidgetCommand(HWND widget) = 0;
 virtual void AppRegister_HandleCommand(HANDLE handle) = 0;
+virtual void AppRegister_IconCommand(HICON icon) = 0;
 virtual void AppRegister_InstanceCommand(HINSTANCE hinstance) = 0;
+virtual void AppRegister_MenuCommand(HMENU menu) = 0;
 virtual void AppRegister_NetCommand(int pCommand) = 0;
 virtual void AppRegister_GlCommand(HGLRC pGlCommand) = 0;
 virtual void AppRegister_DcCommand(HDC pDcCommand) = 0;
+virtual void AppRegister_Command(UINT pCommand) = 0;
 
 };
 
