a0a2bfc6dc1c852412d21753a62ea9ddaea2f416
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-31 10:24:05 -0600

    Update Csg.cpp

---
+++ source/EditorCommon/Csg.cpp
@@ -0,0 +1,43 @@
+//Csg.cpp includes
+#include <limits.h>
+#include "Stdafx.h"
+
+//globals
+static int csgId = 0;
+
+#define CSG_HOLLOW 000x000
+#define CSG_GROUP 000x001
+
+std::size_t csgsize;
+
+class Csg{
+public:
+ Csg();
+ ~Csg();
+
+ int numberid;//set to id int
+
+ qboolean csgdrawn;
+ qboolean csgscale;
+ qboolean csgtranslate;
+ qboolean csgrotate;
+
+ virtual void DrawCsg(Csg * csg) = 0;
+ virtual void CreateCsg(Csg& csg) = 0;
+ int CsgMouseRegister(Csg& csg, int mouse_button);
+ virtual void RegisterMouse(int * Mouse) = 0;
+
+ int MAX_CSG_FACES[9999];
+
+ virtual void LeftClckMB() = 0;
+ virtual void RightClckMB() = 0;
+ virtual void MiddleClckMB() = 0;
+ virtual void DestroyCsg() = 0;
+
+ qboolean texdef_csg_enabled;
+
+ enum{
+
+ };
+
+};
