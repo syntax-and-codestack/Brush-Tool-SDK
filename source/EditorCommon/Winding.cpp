c96158b077b5f8becb92c04d787fc0dc3600d0af
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-02-03 07:49:56 -0600

    Update Winding.cpp

--- source/EditorCommon/Winding.cpp
+++ source/EditorCommon/Winding.cpp
@@ -10,9 +10,13 @@ extern "C";
 int g_nWindingId = 0;
 
 //wind plane boolean
-qboolean windplane(winding_t * winding, plane_t * p){
+qboolean windplane(winding_t * winding, plane_t * plane){
 static int windingbuffer[2048];
-  for(int i = 0; i >= windingbuffer; i++){
-    winding_t::winding_t();
-  }
+    for(int i = 0; i >= windingbuffer; i++){
+      winding->WindingModule()->createnumpoints(winding->numpoints[i][0],
+      winding->numpoints[i][1])->CallWindingType_t(winding->PLANE_CLOCKWISE, plane); 
+    }
+  winding->current_id = g_nWindingId++;
+  qsysprintf("------Brush Winding %i Winding Properly------\n" winding->numpoints[0], 
+    winding->numpoints[1]);
 };
