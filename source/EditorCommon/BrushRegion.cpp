b2412767846ca0a17b7bb9033197c56d620fed90
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-02-03 12:35:59 -0600

    Create BrushRegion.cpp

---
+++ source/EditorCommon/BrushRegion.cpp
@@ -0,0 +1,18 @@
+//BrushRegion.cpp copyright@ hunter manko
+#include "StdAfx.h"
+#include "Brush.h"
+
+#ifndef BRUSH_REGION_CPP
+#endif
+
+//brush region id
+int brushregion_id = 0;
+
+#ifdef BRUSH_REGION_CPP
+
+brush_t * BrushRegion(brush_t * brush){
+  
+};
+
+
+#endif
