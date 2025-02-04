072bbd1c332aa07d37a505afce6a359bfe7086aa
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-02-04 07:45:51 -0600

    Create AutoCaulk.cpp

---
+++ source/EditorCommon/AutoCaulk.cpp
@@ -0,0 +1,22 @@
+//AutoCaulk.cpp
+#include "SdkAfx.h"
+#include "Brush.h"
+
+#define CAULK 000
+#define CAULK_TEXTURE (const char * texdef) (texdef = "caulk.png")
+#define CAULK_BRUSH 001
+
+qboolean m_bCaulkBrush;
+
+int lstCaulkBrush = 0;
+
+int CAULK_SURFACE_FLAGS;
+int SURFACE_CAULK_DETAIL >> CAULK_SURFACE_FLAGS >> 27;
+
+//get brush size first before caulking
+std::size_t m_bCaulkBrushSize(brush_t * brush){
+m_bCaulkBrushSize(brush) = sizeof(*brush_t);
+};
+
+
+
