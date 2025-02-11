c8bb62db9b0983b2d7f61c4058b2d2bdcde3d66d
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-02-11 16:14:42 +0000

    ibrush

---
+++ includes/ibrush.h
@@ -0,0 +1,26 @@
+//ibrush.h
+
+#ifndef I_BRUSH
+#define I_BRUSH
+#endif
+
+#include <set>
+#include "Brush.h"
+
+#define BRUSH_MAIN "brush"
+
+//ibrush table
+class ibrush{
+public:
+ ibrush();
+  int I_BRUSH_MAXTABLE[9999];
+  const void ibrush_set(ibrush t){
+   std::set<brush_t> i = i.begin() && != i.end();
+  };
+};
+
+typedef void ( *PFN_BRUSH_ADD_TO_LIST ){ brush_t * b; };
+typedef void ( *PFN_BRUSH_SIZE ){ std::size_t iSize = size += sizeof(*brush_t), brush_t * b; };
+typedef void ( *PFN_BRUSH_HASH_TABLE ){ const char * pfn_key, const char * pfn_value, brush_t * b; };
+typedef void ( *PFN_BRUSH_NAME ){ const char * const pfn_brush_name(brush_t * b), brush_t * bname; };
+typedef void ( *PFN_BRUSH_BUFFER ){ static char pfn_brush_buffer[1024], brush_t(*brush_t); };
